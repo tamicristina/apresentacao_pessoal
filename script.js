04a631a5c7a7517df2c1e1da14573ae0d99e7fcc
tamicristina <tami.cristina06@gmail.com>
2022-06-05 21:13:03 -0300

    refatorando slider

---
+++ script.js
@@ -0,0 +1,19 @@
+let count = 1;
+
+// let radio = (document.getElementById("radio" + count).checked = true);
+// console.log(radio);
+
+// setInterval(() => {
+//   nextImage();
+// }, 3000);
+
+// function nextImage() {
+//   count++;
+//   if (count > 3) {
+//     count = 1;
+//   }
+
+//   document.getElementById("radio" + count).checked = true;
+// }
+
+// console.log("estou aqui");
