e047fedf4625c0d7d4a3b2cb0dc587e182564caf
tamicristina <tami.cristina06@gmail.com>
2022-06-06 23:36:23 -0300

    formacao e portfolio finalizados

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 let count = 1;
 
-// let radio = (document.getElementById("radio" + count).checked = true);
-// console.log(radio);
+let radio = (document.getElementById("radio" + count).checked = true);
+console.log(radio);
 
-// setInterval(() => {
-//   nextImage();
-// }, 3000);
+setInterval(() => {
+  nextImage();
+}, 3000);
 
-// function nextImage() {
-//   count++;
-//   if (count > 3) {
-//     count = 1;
-//   }
+function nextImage() {
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
 
-//   document.getElementById("radio" + count).checked = true;
-// }
-
-// console.log("estou aqui");
+  document.getElementById("radio" + count).checked = true;
+}
